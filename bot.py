e493640d3228eec55ffb6c71fd3bda429c2a01e4
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-08-01 10:39:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,26 +3,52 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
+
 from database import init_db
 
-from handlers import start, rewards
+from handlers import (
+    start,
+    rewards,
+    luck,
+    codes
+)
 
 
 async def main():
 
     init_db()
 
-    bot = Bot(BOT_TOKEN)
+    bot = Bot(
+        BOT_TOKEN
+    )
 
     dp = Dispatcher()
 
 
-    dp.include_router(start.router)
-    dp.include_router(rewards.router)
+    dp.include_router(
+        start.router
+    )
+
+    dp.include_router(
+        rewards.router
+    )
+
+    dp.include_router(
+        luck.router
+    )
+
+    dp.include_router(
+        codes.router
+    )
+
+
+    print("☂️ ixxy Codes запущен")
 
 
     await dp.start_polling(bot)
 
 
+
 if __name__ == "__main__":
+
     asyncio.run(main())
